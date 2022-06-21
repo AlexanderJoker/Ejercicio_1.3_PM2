3ec989ce9943ddbddd28f100bccf81aa66373dcb
AlexanderJoker <alexandertinoco12349@gmail.com>
2022-06-20 19:49:38 -0600

    Add project files.

---
+++ IngresoPersonasBaseDatos/Controller/DataBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using IngresoPersonasBaseDatos.models;
+using SQLite;
+
+namespace IngresoPersonasBaseDatos.Controller
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection db;
+
+        public Database(String pathdb)
+        {
+            db = new SQLiteAsyncConnection(pathdb);
+            // Creacion de tabla personas 
+            db.CreateTableAsync<models.Personas>().Wait();
+        }
+
+        // CRUD operations with SQLite
+        public Task<List<Personas>> getListPersons()
+        {
+            return db.Table<Personas>().ToListAsync();
+        }
+
+        // Get person by code
+        public Task<Personas> getPersonByCode(int codeParam)
+        {
+            return db.Table<Personas>()
+                .Where(i => i.code == codeParam)
+                .FirstOrDefaultAsync();
+        }
+
+        // Create person
+        public Task<int> savePerson(Personas person)
+        {
+            if (person.code != 0)
+            {
+                return db.UpdateAsync(person);
+            }
+            else
+            {
+                return db.InsertAsync(person);
+            }
+
+        }
+
+        public Task<int> deletePerson(Personas person)
+        {
+            return db.DeleteAsync(person);
+        }
+    }
+}
+
+
---
+++ IngresoPersonasBaseDatos/Views/ActualizarPersona.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IngresoPersonasBaseDatos.Views;
+using IngresoPersonasBaseDatos.models;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IngresoPersonasBaseDatos.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActualizarPersona : ContentPage
+    {
+        public ActualizarPersona()
+        {
+            InitializeComponent();
+        }
+    
+    private async void btnConfirmUpdate_Clicked(object sender, EventArgs e)
+    {
+        var personForUpdating = new Personas()
+        {
+            code = Convert.ToInt32(txtCode.Text),
+            firstNames = txtFirstnames.Text,
+            lastNames = txtLastnames.Text,
+            age = Convert.ToInt32(txtAge.Text),
+            address = txtAddress.Text,
+            email = txtEmail.Text
+        };
+
+        var result = await App.BaseDatos.savePerson(personForUpdating);
+        if (result == 1)
+        {
+            await DisplayAlert("Actualizar", "Actualizado correctamente", "OK");
+            await Navigation.PopAsync();
+        }
+        else
+        {
+            await DisplayAlert("Actualizar", "No se pudo actualizar la información", "OK");
+        }
+
+    }
+
+    private async void btnCancelUpdate_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}
+    }
